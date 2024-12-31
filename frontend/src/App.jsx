d54a975cbb9e93995bab042986b6fe18c94a9690
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-31 08:41:13 -0800

    changing url path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import CreateEmployee from './pages/Clients/CreateEmployee'
+import DisplayFulltime from './pages/Clients/DisplayFulltime'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/employee/create-user' element={<CreateEmployee />} />
+        <Route path='/client/create' element={<CreateEmployee />} />
+        <Route path='/client/display-fulltime' element={<DisplayFulltime />} />
       </Routes>
     </Router>
   )
