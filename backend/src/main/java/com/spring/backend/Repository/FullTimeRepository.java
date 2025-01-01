87f2ab78a9b3d68bebb5b67ec81a1f379d00394a
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2025-01-01 04:18:26 -0800

    rename repository files

---
+++ backend/src/main/java/com/spring/backend/Repository/FullTimeRepository.java
@@ -0,0 +1,10 @@
+package com.spring.backend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.backend.Entity.FullTime;
+
+
+public interface FullTimeRepository extends JpaRepository<FullTime, Long> {
+    
+}
---
+++ backend/src/main/java/com/spring/backend/Repository/PartTimeRepository.java
@@ -0,0 +1,9 @@
+package com.spring.backend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.backend.Entity.PartTime;
+
+public interface PartTimeRepository extends JpaRepository<PartTime, Long>{
+    
+}
