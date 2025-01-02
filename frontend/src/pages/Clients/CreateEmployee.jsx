951a153a92c3fafb3da07ad1a51a5bac4e6fcda8
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2025-01-02 04:04:34 -0800

    fixing error bug

--- frontend/src/pages/Clients/CreateEmployee.jsx
+++ frontend/src/pages/Clients/CreateEmployee.jsx
@@ -1,193 +1,24 @@
 import React, { useState } from "react";
-import axios from "axios";
+import FullTimeRecord from "../../components/FullTimeRecord";
+import PartTimeRecord from "../../components/PartTimeRecord";
 
 function CreateEmployee() {
-  const [account, setAccount] = useState({
-    name: "",
-    position: "",
-    department: "",
-
-    hoursWorked: "",
-    salaryAmount: "",
-    timeIn: "",
-    timeOut: "",
-  });
-
-  const [selectedOption, setSelectedOption] = useState("")
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setAccount((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-
-    setSelectedOption(e.target.value)
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const payload = {
-      name: account.name,
-      position: account.position,
-      department: account.department,
-      fullTimeSalary: {
-        jobType: account.jobType,
-        hoursWorked: account.hoursWorked,
-        salaryAmount: account.salaryAmount,
-        timeIn: account.timeIn,
-        timeOut: account.timeOut,
-      },
-    };
-
-    try {
-      const response = await axios.post("/api/create", payload);
-
-      console.log("Response from server:", response.data);
-      alert("New record has been sent");
-
-      setAccount({
-        name: "",
-        position: "",
-        department: "",
-        jobType: "",
-        hoursWorked: "",
-        salaryAmount: "",
-        timeIn: "",
-        timeOut: "",
-      });
-    } catch (error) {
-      console.error(
-        "Error during request:",
-        error.response ? error.response.data : error.message
-      );
-      alert("Failed to submit data.");
-    }
-  };
+  // State for the select option
+  const [selectedOption, setSelectedOption] = useState("FullTimeRecord");
 
   return (
-    <div className="flex justify-center items-center">
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <h1 className="text-xl">Employee Record</h1>
-        <label className="input input-bordered flex items-center gap-2">
-          Name:
-          <input
-            type="text"
-            className="grow"
-            name="name"
-            value={account.name}
-            onChange={handleChange}
-            placeholder="John Doe"
-          />
-        </label>
-        <label className="input input-bordered flex items-center gap-2">
-          Position:
-          <input
-            type="text"
-            className="grow"
-            name="position"
-            value={account.position}
-            onChange={handleChange}
-            placeholder="IT Instructor"
-          />
-        </label>
-        <label className="input input-bordered flex items-center gap-2">
-          Department:
-          <input
-            type="text"
-            className="grow"
-            name="department"
-            value={account.department}
-            onChange={handleChange}
-            placeholder="Computer Studies"
-          />
-        </label>
-        <label className="flex justify-center gap-2">
-          <form className="flex justify-between gap-6">
-            <div className="flex items-center gap-2">
-              <span>Full Time</span>
-              <input
-                type="radio"
-                className="radio radio-sm"
-                name="jobType"
-                value="Full Time"
-                onChange={handleChange}
-                checked={selectedOption === "Full Time"}
-              />
-            </div>
-            <div className="flex items-center gap-2">
-              <span>Part Time</span>
-              <input
-                type="radio"
-                className="radio radio-sm"
-                name="jobType"
-                value="Part Time"
-                onChange={handleChange}
-                checked={selectedOption === "Part Time"}
-              />
-            </div>
-          </form>
-        </label>
+    <div className="flex flex-col justify-center items-center">
+      <select
+        className="select select-sm select-bordered w-full max-w-xs"
+        onChange={(e) => setSelectedOption(e.target.value)}
+        value={selectedOption}
+      >
+        <option value="FullTimeRecord">Full Time</option>
+        <option value="PartTimeRecord">Part Time</option>
+      </select>
 
-        {/* Fulltime Base */}
-        <div className="flex justify-between gap-4">
-          <label className="input input-bordered flex items-center gap-2">
-            Working Time:
-            <input
-              type="number"
-              className="grow"
-              name="hoursWorked"
-              value={account.hoursWorked}
-              onChange={handleChange}
-              placeholder="8"
-            />
-            <p>Hours</p>
-          </label>
-          <label className="input input-bordered flex items-center gap-2">
-            Salary Amount:
-            <input
-              type="number"
-              className="grow"
-              name="salaryAmount"
-              value={account.salaryAmount}
-              onChange={handleChange}
-              placeholder="24000"
-            />
-            <p>Pesos</p>
-          </label>
-        </div>
-        <div className="flex justify-between gap-4">
-          <label className="input input-bordered flex items-center gap-2">
-            Time In:
-            <input
-              type="time"
-              className="grow"
-              name="timeIn"
-              value={account.timeIn}
-              onChange={handleChange}
-              placeholder="6:00"
-            />
-          </label>
-          <label className="input input-bordered flex items-center gap-2">
-            Time Out:
-            <input
-              type="time"
-              className="grow"
-              name="timeOut"
-              value={account.timeOut}
-              onChange={handleChange}
-              placeholder="12:00"
-            />
-          </label>
-        </div>
-
-        <input
-          type="submit"
-          className="btn btn-success text-white"
-          value="Create"
-        />
-      </form>
+      {selectedOption === "FullTimeRecord" && <FullTimeRecord />}
+      {selectedOption === "PartTimeRecord" && <PartTimeRecord />}
     </div>
   );
 }
--- frontend/src/pages/Clients/DisplayFulltime.jsx
+++ frontend/src/pages/Clients/DisplayFulltime.jsx
@@ -1,146 +1,62 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import FullTimeTable from "../../components/FullTimeTable";
+import PartTimeTable from "../../components/PartTimeTable";
 
 function DisplayFulltime() {
-  const [employees, setEmployees] = useState([]);
+  const [selectedType, setSelectedType] = useState("FullTime");
 
-  useEffect(() => {
-    const fetchEmployees = async () => {
-      try {
-        const response = await axios.get("/api/get/fulltime");
-        setEmployees(response.data);
-      } catch (error) {
-        console.error("Error fetching employees:", error);
-      }
-    };
-
-    fetchEmployees();
-  }, []);
+  const handleFilterChange = (event) => {
+    setSelectedType(event.target.value);
+  };
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="flex justify-between w-full px-8">
-        <div>
-          <select className="select select-sm select-bordered w-full max-w-xs">
+    <div className="flex flex-col gap-6 justify-center items-center">
+      <h2>Record List</h2>
+      {/* Dropdown Option */}
+      <div className="flex justify-between w-1/2">
+        <div className="flex items-center gap-2">
+          <label htmlFor="employeeType" className="text-lg">
+            Filter Job Type:{" "}
+          </label>
+          <select
+            id="employeeType"
+            className="select select-bordered select-sm  "
+            value={selectedType}
+            onChange={handleFilterChange}
+          >
             <option disabled selected>
-              Filter
+              Job Type
             </option>
-            <option>Full Time</option>
-            <option>Part Time</option>
+            <option value="FullTime">FullTime</option>
+            <option value="PartTime">PartTime</option>
           </select>
         </div>
-        <div>
-          <label className="input input-sm input-bordered flex items-center gap-2">
+        <div className="flex items-center gap-2">
+          Search:
+          <label
+            className="input input-bordered input-sm flex items-center gap-2"
+            htmlFor="search"
+          >
+            <input type="text" className="grow" placeholder="John Doe" />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
               fill="currentColor"
               className="h-4 w-4 opacity-70"
             >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
             </svg>
-            <input type="text" className="grow" placeholder="Search Name" />
           </label>
         </div>
       </div>
-      <div className="overflow-x-auto">
-        <table className="table text-center">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Job Type</th>
-              <th>Salary</th>
-              <th>Worked Hours</th>
-              <th>Schedule</th>
-              <th>Settings</th>
-            </tr>
-          </thead>
-          <tbody>
-            {employees.map((employee) => (
-              <tr key={employee.id}>
-                <td className="font-semibold">{employee.name}</td>
-                <td className="font-semibold">
-                  {employee.position}
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    {employee.department}
-                  </span>
-                </td>
-                <td className="font-semibold">
-                  {employee.fullTimeSalary.jobType}
-                </td>
-                <td className="font-semibold">
-                  {employee.fullTimeSalary.salaryAmount}
-                </td>
-                <td className="font-semibold">
-                  {employee.fullTimeSalary.hoursWorked}
-                </td>
-                <td className="font-semibold">
-                  <div className="flex justify-between gap-4">
-                    <div>{employee.fullTimeSalary.timeIn}AM</div>-
-                    <div>{employee.fullTimeSalary.timeOut}PM</div>
-                  </div>
-                </td>
-                <td className="font-semibold">
-                  <div className="flex justify-between gap-4">
-                    <div></div>
-                    <div>
-                      <button
-                        type="button"
-                        className="btn btn-primary btn-xs text-white"
-                      >
-                        Update
-                      </button>
-                    </div>
-                    <div>
-                      <button
-                        type="button"
-                        className="btn btn-error btn-xs text-white"
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
 
-              // <tr key={employee.id}>
-              //   <td>
-              //     <div className="flex items-center gap-3">
-              //       <div className="avatar">
-              //         <div className="mask mask-squircle h-12 w-12">
-              //           <img
-              //             src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-              //             alt="Avatar Tailwind CSS Component"
-              //           />
-              //         </div>
-              //       </div>
-              //       <div>
-              //         <div className="font-bold">{employeee.name}</div>
-              //       </div>
-              //     </div>
-              //   </td>
-              //   <td>
-              //     {employee.position}
-              //     <br />
-              //     <span className="badge badge-ghost badge-sm">
-              //       {employee.department}
-              //     </span>
-              //   </td>
-              //   <td>{employee.fullTimeSalary.hoursWorked}</td>
-              //   <td>{employee.fullTimeSalary.salaryAmount}</td>
-              //   <td>
-              //     <div className="flex justify-between gap-4">
-              //       {employee.fullTimeSalary.timeIn}
-              //       {employee.fullTimeSalary.timeOut}
-              //     </div>
-              //   </td>
-              // </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {/* Table Records */}
+      {selectedType === "FullTime" && <FullTimeTable />}
+      {selectedType === "PartTime" && <PartTimeTable />}
     </div>
   );
 }
