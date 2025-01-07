62b3bebdd57c926b6fec8d023abcf79c717571cc
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2025-01-07 02:30:57 -0800

    deleted files

--- backend/src/main/java/com/spring/backend/Repository/UserAccountFullTimeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.backend.Entity.UserAccountFullTime;
-
-public interface UserAccountFullTimeRepository extends JpaRepository<UserAccountFullTime, Long> {
-    
-}
