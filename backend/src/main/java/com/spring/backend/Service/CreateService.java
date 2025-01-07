57147ce98286ceb61a15697a2d0354ffb2bfc101
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2025-01-07 03:16:10 -0800

    adding feature that calculate rate and time for part time

--- backend/src/main/java/com/spring/backend/Service/CreateService.java
+++ backend/src/main/java/com/spring/backend/Service/CreateService.java
@@ -1,5 +1,10 @@
 package com.spring.backend.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +21,28 @@ public class CreateService {
 
     @Autowired
     private PartTimeRepository partTimeRepository;
-    
-    public FullTime createFullTime (FullTime fullTime) {
+
+    public FullTime createFullTime(FullTime fullTime) {
         return fullTimeRepository.save(fullTime);
     }
 
-    public PartTime createPartTime (PartTime partTime) {
+    public PartTime createPartTime(PartTime partTime) {
+        double wage = calculateWage(partTime.getRate(), partTime.getTimeIn(), partTime.getTimeOut());
+        BigDecimal formattedWage = formatWage(wage);
+        partTime.setWage(formattedWage.doubleValue());
         return partTimeRepository.save(partTime);
+    }
+
+    public double calculateWage(double rate, String timeIn, String timeOut) {
+        LocalTime inTime = LocalTime.parse(timeIn);
+        LocalTime outTime = LocalTime.parse(timeOut);
+        long hoursWorked = ChronoUnit.HOURS.between(inTime, outTime);
+        return rate * hoursWorked;
+    }
+
+    private BigDecimal formatWage(double wage) {
+        BigDecimal wageBD = new BigDecimal(wage).setScale(2, RoundingMode.HALF_UP);
+        return wageBD;
     }
 
 }
